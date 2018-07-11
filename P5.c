3d8ce5c5b1d684e254999dcd0709273f2e6bb05f
heitorhvc <37555708+heitorhvc@users.noreply.github.com>
2018-07-11 15:29:01 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,51 +1,52 @@
 //Heitor Henrique Vasconcelos Carneiro 
 //11711EEL024
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
-{
-	int j , i,num[64], *pnum;
-	char msg[256], *pmsg;
+{	
+	char str[256], *c;
+	int	caso, vetor[63],*p, i;
+	p=(int*)str;
+	c=(char*)vetor;
+	do{	printf("\n\n\tDes-Codifica \n\n Escolha que tipo de operacao deseja fazer:\n1. Codificar"
+		scanf("%d", &caso); getchar();
 	
-	for(i = 0; i < 256; i++) msg[i] = '\0';
-	printf("\t1Para Codificar a mensagem\n\t2 Para Decodificar a frase\n\t Escolha uma das opcoes: ");
-	scanf("%d",&j);
-	getchar();
-	
-	switch(j)
-	{
-		
-		case 1:{
-			printf("\tLimite maximo de caracteres eh de 255: \n\n\t");//indica quantidade maxima de caracteres 
-			scanf("%s", (char*) msg);//varre a mensagem
-			char *pmsg= msg;
-			printf("\n\t Frase ja codificada:\n\t\t");//mostra para usuario
-			
-			for(i=0;msg[i];i++)
-			{//laco para codificar
-				pnum = (int*)pmsg;
-				if(pnum[i]!=0) printf("%d",pnum[i]);
-				if(pnum[i+1]!=0) printf(", ");
-				}
-			break;//pausa
-		}
-		
-		case 2:
+		switch(caso)
 		{
-			printf("\tDigite os numeros sendo o maximo de 64 algarismos.O ultimo algarismo (64) entre 1 e ""4095!!!\n   \t\tObs:Digite a letra 0 p/ finalizar a entrada de numeros\n\n\n]");//explicando funcao do programa
-			
-			for(i=0;i<64;i++)//laco para varrer numeros
-			 {
-				printf("\tn%d:\t ", i+1); scanf("%d", &num[i]);//mostrar e ler numeros
-				pmsg = (char*)num;
-				if(num[i]==0) //laço para decodificacao
+			case 1:
+				
+				printf("\n Entre com uma mensagem de ate 255 caracteres:\n");
+				fgets(str,256, stdin);
+				//getchar();
+				for (i=0;i<sizeof(str);i++)
+				{
+					printf("%i ",p[i]);
+					if(p[i]==0)
+					break;
+				}			
 				break;
-			 }
-			 
-			 printf("\n\n\tFrase decodificada: %s", pmsg);//mostra frase decodificada
-	    }
-	    
+			case 2:
+				printf("\n  Entre com um codigo numerico de ate 64 numeros:\n"
+				for (i=0;i<64;i++)
+				{
+					vetor[i]=0;
+				}
+				for (i=0;i<64;i++)
+				{
+					scanf("%d",&vetor[i]);
+					if (vetor[i]==0)
+					{break;}
+				}
+				printf("\n\n");
+				for (i=0;i<sizeof(vetor);i++)
+				{
+					printf("%c",c[i]);
+				}
+				break;
 			
-	}}
-
-
+			case 3:
+				return 0;
+				break;
+			}
+	}while(caso!=3);		
+	return 0;
+}
